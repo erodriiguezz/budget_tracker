224592241958a70aaa7d402e88c123f4bb10d5a5
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-05-17 22:35:58 -0400

    added mlab uri

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://localhost/budget" ||
+    "mongodb://ernesto:Corona7070@ds115396.mlab.com:15396/heroku_n2p080c5",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
